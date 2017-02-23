d37e87b93527345b562ff8a89d43557471f304b0
Hari Vedam <hvedam@gmail.com>
2017-02-22 18:11:38 -0600

    updated tournament sql

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -7,14 +7,12 @@
 -- these lines here.
 
 -- Create tournament database
-create database tournament;
-\c tournament;
 
 -- Drop Database if EXISTS
 drop database IF EXISTS tournament;
--- Drop Tables if they exist
-drop table IF EXISTS Players;
-drop table IF EXISTS Matches;
+--Create the tournament
+create database tournament;
+\c tournament;
 
 -- Creating Tables
 create table Players (
@@ -29,3 +27,7 @@ create table Matches (
   winner serial references Players(players_id)
   loser serial references Players(players_id)
 );
+
+-- Drop Tables if they exist
+drop table IF EXISTS Players;
+drop table IF EXISTS Matches;
